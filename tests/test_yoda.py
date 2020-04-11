171b847a281bd9be816d03e4754fbdb11bc35f25
Antti Ajanki <antti.ajanki@iki.fi>
2020-04-11 21:54:03 +0300

    XSV order

--- tests/test_yoda.py
+++ tests/test_yoda.py
@@ -1,8 +1,12 @@
-from yoda.yoda import reorder_osv
+from yoda.yoda import reorder_osv, reorder_xsv
 
+
+simple_sentences = [
+    'Talo on punainen.',
+    'Asiat olivat paremmin.',
+]
 
 sentences_without_object = [
-    'Talo on punainen.',
     'Eduskunnan rooli tässä työssä on tärkeä.',
     'Ainakin neljä uhkatekijää nousee heti esiin.',
     'Miksi et ymmärrä?',
@@ -41,10 +45,40 @@ def test_reorder_osv_object_with_relative_clause():
 
 
 def test_reorder_osv_object_in_independent_clause():
-    assert (reorder_osv('Ihmiset kokoontuvat toreilla ja juhlivat voittoa.') ==
-            'Ihmiset kokoontuvat toreilla ja juhlivat voittoa.')
+    s = 'Ihmiset kokoontuvat toreilla ja juhlivat voittoa.'
+    assert reorder_osv(s) == s
+
+
+def test_reorder_osv_simple_sentences():
+    for s in simple_sentences:
+        assert reorder_osv(s) == s
 
 
 def test_reorder_osv_no_change_without_object():
     for s in sentences_without_object:
+        assert reorder_osv(s) == s
+
+
+def test_reorder_xsv_xcomp():
+    assert (reorder_xsv('Pyysin sinua vastaamaan!') ==
+            'Sinua vastaamaan pyysin!')
+
+    assert (reorder_xsv('Hän yritti suorittaa tehtävää ilman suurempaa innostusta.') ==
+            'Suorittaa tehtävää ilman suurempaa innostusta hän yritti.')
+
+
+def test_reorder_xsv_advmod():
+    assert reorder_xsv('Etene varovasti!') == 'Varovasti etene!'
+    
+    assert (reorder_xsv('Avaimet olivat kadoksissa eilen.') ==
+            'Eilen avaimet olivat kadoksissa.')
+
+
+def test_reorder_xsv_start_with_advmod():
+    s = 'Huomenna hän tulee'
+    assert reorder_xsv(s) == s
+
+
+def test_reoder_xsv_simple_sentences():
+    for s in simple_sentences:
         assert reorder_osv(s) == s
--- yoda/yoda.py
+++ yoda/yoda.py
@@ -1,4 +1,6 @@
 import spacy
+from spacy.tokens.doc import Doc
+from spacy.tokens.token import Token
 
 
 nlp = spacy.load('fi_experimental_web_md')
@@ -6,33 +8,57 @@ nlp = spacy.load('fi_experimental_web_md')
 
 def reorder_osv(sentence: str) -> str:
     """Reorder the words in a sentence in the object-subject-verb order."""
-
     doc = nlp(sentence)
-    obj = find_obj(doc)
-    if obj is None:
-        return sentence
+    obj = find_child_dep(doc, ['obj'])
+    return reorder_subtree(doc, obj)
 
-    obj_span = doc[obj.left_edge.i : obj.right_edge.i + 1]
-    obj_text = obj_span.text
-    obj_text = obj_text[0].upper() + obj_text[1:]
 
-    right_start_token = doc[obj.right_edge.i+1]
+def reorder_xsv(sentence: str) -> str:
+    """Reorder non-object complement to the verb as the first term.
+
+    This is the "XSV order" where X is something else that the object.
+
+    (obl)
+    Etsimme taskulampun kanssa. -> Taskulampun kanssa etsimme.
+
+    (xcomp)
+    Oletin sinun muuttavan mieltäsi. -> Sinun mieltäsi muuttavan oletin.
+    
+    (advmod)
+    Katsoin eilen. -> Eilen katsoin.
+    """
+    doc = nlp(sentence)
+    subtree = find_child_dep(doc, ['xcomp', 'obl', 'advmod'])
+    return reorder_subtree(doc, subtree)
+
+
+def reorder_subtree(doc: Doc, subtree: Token) -> str:
+    if subtree is None:
+        return doc.text
+
+    span = doc[subtree.left_edge.i : subtree.right_edge.i + 1]
+    subtree_text = span.text
+    subtree_text = subtree_text[0].upper() + subtree_text[1:]
+
+    right_start_token = doc[subtree.right_edge.i+1]
     sep = '' if right_start_token.is_punct or right_start_token.is_quote else ' '
-    rest_text = doc[:obj.left_edge.i].text + sep + doc[obj.right_edge.i+1:].text
+    rest_texts = [
+        doc[:subtree.left_edge.i].text,
+        doc[subtree.right_edge.i+1:].text
+    ]
+    rest_texts = [x for x in rest_texts if x]
+    rest_text = sep.join(rest_texts)
     rest_text = rest_text[0].lower() + rest_text[1:]
 
-    sep = ', ' if any(t.text == ',' for t in obj_span) else ' '
-    return obj_text + sep + rest_text
+    sep2 = ', ' if any(t.text == ',' for t in span) else ' '
+    return subtree_text + sep2 + rest_text
 
 
-def find_obj(doc):
-    root = [t for t in doc if t.dep_ == 'ROOT']
-    if not root:
-        return None
-
-    root = root[0]
-    for t in root.children:
-        if t.dep_ == 'obj':
-            return t
+def find_child_dep(doc, deps):
+    for t in doc:
+        if t.dep_ == 'ROOT':
+            for child in t.children:
+                if child.dep_.split(':', 1)[0] in deps:
+                    return child
 
     return None
