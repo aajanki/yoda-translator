3726845de1e23b8d0a7a6c8d07423a0e1fad0369
Antti Ajanki <antti.ajanki@iki.fi>
2020-04-15 21:19:35 +0300

    attr dependency

--- tests/test_yoda_en.py
+++ tests/test_yoda_en.py
@@ -7,3 +7,4 @@ y = YodaEn()
 def test_en():
     assert y('You have become powerful.') == 'Powerful you have become.'
     assert y('I sense the dark side in you.') == 'The dark side in you I sense.'
+    assert y('You are a wizard, Harry!') == 'A wizard, Harry, you are!'
